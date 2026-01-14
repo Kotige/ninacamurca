7cbc854bcc962ef6ed332df3539a86b4bae3ef8e
Vitor <vitor_fernandes_pie@yahoo.com.br>
2026-01-14 12:10:05 -0300

    fix: feedback gallery

--- src/Pages/Home/Feedbacks/index.jsx
+++ src/Pages/Home/Feedbacks/index.jsx
@@ -1,6 +1,6 @@
 import TitleH2 from "../../../Components/TitleH2";
-import GalleryDesktop from "../../../Components/GalleryDesktop";
-import ImageMosaic from "../../../Components/ImageMosaic";
+import feedbacklg from "../../../assets/images/feed-desktop.png"
+import FeedCarousel from "../../../Components/FeedCarousel";
 
 import "./styles.scss"
 
@@ -10,10 +10,13 @@ export default function Feedback() {
     return (
         <>
             <section id="feedbacks" className="row justify-content-center align-items-center">
-                <div className="d-block d-lg-none gallery-mobile">
-                    <TitleH2 className="title title-mobile">
-                        O que dizem sobre <br /> meu trabalho
-                    </TitleH2>
+                <div className="d-block d-lg-none m-5 gallery-mobile">
+                    <div className="mobile-feed-container py-4 px-3">
+                        <TitleH2 className="title title-mobile">
+                            O que dizem sobre <br /> meu trabalho
+                        </TitleH2>
+                        <FeedCarousel />
+                    </div>
 
                 </div>
                 <div className="d-none d-lg-block gallery-desktop">
@@ -23,16 +26,8 @@ export default function Feedback() {
                     </TitleH2>
 
                     <div className="feedback-container d-flex">
-                        <div className="gallery-content p-4">
-                            {/* <GalleryDesktop /> */}
-                            <ImageMosaic 
-                                images={[
-                                    "/gallery/feedback1.jpeg",
-                                    "/gallery/feedback2.jpeg",
-                                    "/gallery/feedback3.jpeg",
-                                    "/gallery/feedback4.jpeg"
-                                ]}
-                            />
+                        <div className="gallery-content">
+                            <img src={feedbacklg} alt="" className="img-fluid" />
                         </div>
                         <div className="div-palette">
                         </div>
